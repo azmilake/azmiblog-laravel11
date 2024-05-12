9946813c28993fd5be8767257c0746fce2e61728
azmilake <githubazmi99@gmail.com>
2024-05-12 15:00:11 +0700

    Menginstall tailwind css pada project

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,11 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Blog</title>
 </head>
 
 <body>
-    <h1>{{ $name }} Home</h1>
+    <article>
+        <h2>{{ $title }}</h2>
+        <p>{{ $body }}</p>
+    </article>
+
+    <article>
+        <h2>{{ $title }}</h2>
+        <p>{{ $body }}</p>
+    </article>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/azmi', function () {
     return view('home', [
-        'name' => 'Muhammad Azmi'
+        'name' => 'Muhammad Azmi',
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog', [
+        'title' => 'New Article Title',
+        'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptates iste odio repellendus. Modi porro
+        cum optio totam aliquid provident vel autem vitae iste temporibus. Magnam asperiores a veniam itaque enim
+        harum molestias neque id quam. Perspiciatis nam exercitationem eveniet?',
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', [
+        'email' => 'mazmi9999@gmail.com',
+        'phone' => '0987 0986 0888',
     ]);
 });
