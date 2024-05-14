b53e076dfaa4d7bb75c63e23f75650fa6ae0e12f
azmilake <githubazmi99@gmail.com>
2024-05-14 14:04:46 +0700

    Mengubah parameter id menjadi slug pada url

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,14 @@
 <x-layout><x-slot:title>{{ $title }}</x-slot:title>
     @foreach ($posts as $post)
         <article class="w-1/2 border-b-2 pb-4 mb-6">
-            <a href="/posts/{{ $post['id'] }}" class="hover:text-gray-500">
+            <a href="/posts/{{ $post['slug'] }}" class="hover:text-gray-500">
                 <h3 class="text-2xl font-medium">{{ $post['title'] }}</h3>
             </a>
             <div class="text-gray-500 text-sm py-2">
                 <a href="#" class="hover:underline">{{ $post['author'] }}</a> | 5 Mei 2024
             </div>
             <p class="text-base text-gray-800 mb-4">{{ Str::limit($post['body'], 150) }}</p>
-            <a href="/posts/{{ $post['id'] }}" class="text-gray-500 text-sm hover:underline">Read &raquo;</a>
+            <a href="/posts/{{ $post['slug'] }}" class="text-gray-500 text-sm hover:underline">Read &raquo;</a>
         </article>
     @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ Route::get('/posts', function () {
         'title' => 'Blog',
         'posts' => [
             [
-                'id' => 1,
+                'slug' => 'post-title-1',
                 'title' => 'post title 1',
                 'author' => 'Azmi Muhammad',
                 'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat cupiditate saepe doloribus nihil quasi quod enim! Id rem ipsam ea officia. Rerum odio reiciendis, nisi delectus obcaecati quam animi at magni, voluptatibus natus explicabo, excepturi alias cupiditate asperiores quaerat!',
             ],
             [
-                'id' => 2,
+                'slug' => 'post-title-2',
                 'title' => 'post title 2',
                 'author' => 'Anton Bee',
                 'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat cupiditate saepe doloribus nihil quasi quod enim! Id rem ipsam ea officia. Rerum odio reiciendis, nisi delectus obcaecati quam animi at magni, voluptatibus natus explicabo, excepturi alias cupiditate asperiores quaerat!',
@@ -29,24 +29,24 @@ Route::get('/posts', function () {
     ]);
 });
 
-Route::get('posts/{id}', function ($id) {
+Route::get('posts/{slug}', function ($slug) {
     $posts = [
         [
-            'id' => 1,
+            'slug' => 'post-title-1',
             'title' => 'post title 1',
             'author' => 'Azmi Muhammad',
             'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat cupiditate saepe doloribus nihil quasi quod enim! Id rem ipsam ea officia. Rerum odio reiciendis, nisi delectus obcaecati quam animi at magni, voluptatibus natus explicabo, excepturi alias cupiditate asperiores quaerat!',
         ],
         [
-            'id' => 2,
+            'slug' => 'post-title-2',
             'title' => 'post title 2',
             'author' => 'Anton Bee',
             'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat cupiditate saepe doloribus nihil quasi quod enim! Id rem ipsam ea officia. Rerum odio reiciendis, nisi delectus obcaecati quam animi at magni, voluptatibus natus explicabo, excepturi alias cupiditate asperiores quaerat!',
         ],
     ];
 
-    $post = Arr::first($posts, function($post) use ($id) { //fitur dari php 8 menggunakan use ($id) untuk menggunakan variabel di luar scope function, karena $id milik dari parent functionnya bukan local functionnya
-        return $post['id'] == $id; 
+    $post = Arr::first($posts, function($post) use ($slug) { //fitur dari php 8 menggunakan use ($slug) untuk menggunakan variabel di luar scope function, karena $id milik dari parent functionnya bukan local functionnya
+        return $post['slug'] == $slug; 
     });
 
     return view('post', [
