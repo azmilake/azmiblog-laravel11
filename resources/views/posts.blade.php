75063bd040141c3f1dd417828542a2d96fac9565
azmilake <githubazmi99@gmail.com>
2024-05-15 13:46:29 +0700

    Menambahkan migration untuk table posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @foreach ($posts as $post)
         <article class="w-1/2 border-b-2 pb-4 mb-6">
             <a href="/posts/{{ $post['slug'] }}" class="hover:text-gray-500">
-                <h3 class="text-2xl font-medium">{{ $post['title'] }}</h3>
+                <h3 class="text-3xl font-semibold">{{ $post['title'] }}</h3>
             </a>
             <div class="text-gray-500 text-sm py-2">
                 <a href="#" class="hover:underline">{{ $post['author'] }}</a> | 5 Mei 2024
